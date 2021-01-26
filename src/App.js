6f77c6fccfc9ed626971dcbe6cdd5d41ed5291b8
Erik Scow <erikscow@gmail.com>
2021-01-26 13:48:55 -0800

    added some new layout content and started styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import React from 'react'
 import './App.css';
+
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      In Development ...
-    </div>
+      <div className="App">
+        <Header/>
+      </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Erik Scow</h1>
+            <nav>
+                <a name='home'>Home</a>
+                <a name='projects'>Projects</a>
+                <a name='about'>About Me</a>
+                <a name='contact'>Contact</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
